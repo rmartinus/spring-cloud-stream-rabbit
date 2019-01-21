b7f12326312ca1b6c52030286702438d51397050
Robbie Martinus <12573669+rmartinus@users.noreply.github.com>
2019-01-22 10:24:59 +1100

    Initial project setup

---
+++ src/main/java/com/cloud/stream/rabbit/cloudstreamrabbit/CloudStreamRabbitApplication.java
@@ -0,0 +1,14 @@
+package com.cloud.stream.rabbit.cloudstreamrabbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudStreamRabbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudStreamRabbitApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/cloud/stream/rabbit/cloudstreamrabbit/CloudStreamRabbitApplicationTests.java
@@ -0,0 +1,17 @@
+package com.cloud.stream.rabbit.cloudstreamrabbit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CloudStreamRabbitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
